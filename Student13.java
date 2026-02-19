cf82c70598f1a71043cc81947c5df21bf2d6c6e8
luciakusumaw <itsmelucia27@gmail.com>
2026-02-19 09:51:45 +0700

    Practicum 1

---
+++ Student13.java
@@ -0,0 +1,38 @@
+public class Student13 {
+    String name;
+    String nim;
+    String kelas;
+    double gpa;
+
+    void showInformation() {
+        System.out.println("Name: " + name);
+        System.out.println("NIM: " + nim);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Class: " + kelas);
+    }
+
+    void changeClass(String newClass) {
+        kelas = newClass;
+    }
+
+    void updateGpa(double newGPA) {
+        gpa = newGPA;
+        if (gpa>=0.0 || gpa<=4.0) {
+        System.out.println("GPA is invalid. Must be between 0.0 and 4.0"); 
+        }
+    }
+
+
+    String performanceValue() {
+        if (gpa >= 3.5) {
+            return "Your Performance is Verry Good";
+        } else if (gpa>=3.0) {
+            return "Your Perormance is Good";
+        } else if (gpa>=2.0) {
+            return "Your Performance is Okay";
+        } else {
+            return "Your Performance is Bad";
+        }
+
+    }
+}
